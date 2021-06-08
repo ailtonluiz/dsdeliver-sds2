783253279a15bbe567caadbf56536db0b2f7bf3b
Ailton Luiz <ailtonatn@gmail.com>
2021-06-08 20:57:34 +0200

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  return (
-    <div>
-     <Navbar />
-    </div>
-  );
+    return (
+        <>
+            <Navbar/>
+            <Home/>
+        </>
+    );
 }
 
 export default App;
