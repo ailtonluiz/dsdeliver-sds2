b24900a1d42f7007552caea888385186ecdf771f
Ailton Luiz <ailtonatn@gmail.com>
2021-06-09 09:55:09 +0200

    Add routes and StepsHeaders

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import StepsHeaders from "./StepsHeaders";
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            <StepsHeaders />
+        </div>
+
+    )
+
+
+}
+
+export default Orders;
